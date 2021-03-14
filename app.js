4857c365cdbfbfc9d8e0b87a1cef6f057e6fc0c0
junaith <junaith81@gmail.com>
2021-03-14 18:26:06 +0800

    Signed-off-by: junaith <junaith81@gmail.com>

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ var helmet = require('helmet');
 
 //  --------------------------------------------------- Begin Database Settings -----------------------------------------------------------------------
 var db = new sqlite3.Database('./database/jandj.db');
-db.run('CREATE TABLE IF NOT EXISTS user("id"	INTEGER NOT NULL,"userName"	TEXT NOT NULL,"password"	TEXT NOT NULL,"userGroup"	TEXT NOT NULL,PRIMARY KEY("id" AUTOINCREMENT)');
-db.run('CREATE TABLE IF NOT EXISTS devices("id"	INTEGER NOT NULL,"device"	TEXT NOT NULL,	"os"	TEXT NOT NULL,"manufacturer"	TEXT NOT NULL,"lastCheckedOutDate"	TEXT,"lastCheckedOutBy"	TEXT,"isCheckedOut"	TEXT NOT NULL,PRIMARY KEY("id" AUTOINCREMENT)');
+db.run('CREATE TABLE IF NOT EXISTS user(id	INTEGER NOT NULL ,userName	TEXT NOT NULL, password	TEXT NOT NULL,userGroup	TEXT NOT NULL,PRIMARY KEY(id AUTOINCREMENT))');
+db.run('CREATE TABLE IF NOT EXISTS devices("id"	INTEGER NOT NULL,"device"	TEXT NOT NULL,	"os"	TEXT NOT NULL,"manufacturer"	TEXT NOT NULL,"lastCheckedOutDate"	TEXT,"lastCheckedOutBy"	TEXT,"isCheckedOut"	TEXT NOT NULL,PRIMARY KEY("id" AUTOINCREMENT))');
 
 //  --------------------------------------------------- End Database Settings -----------------------------------------------------------------------
 
@@ -32,55 +32,184 @@ app.use(helmet());
 //app.use(limiter);
 
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, './public/form.html'));
-});
+/* app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+}); */
 
 //  --------------------------------------------------- End App Settings ---------------------------------------------------------------------------
 
 
 //  --------------------------------------------------- Begin User CRUD Functions -----------------------------------------------------------------------
 // Add New User
-app.post('/Add', function (req, res) {
+
+app.post('/AddUser', function (req, res) {
   db.serialize(() => {
-    db.run('INSERT INTO user(userName,password) VALUES(?,?)', [req.body.userName, req.body.password], function (err) {
-      if (err) {
-        return console.log(err.message);
-      }
-      console.log("New user has been added");
-      res.send("New employee has been added into the database with ID = " + req.body.userName);
-      //alert("New employee has been added into the database with ID = " + req.body.id + " and Name = " + req.body.name);
-    });
+    db.run('INSERT INTO user(userName,password,userGroup) VALUES(?,?,?)',
+      [req.body.userName, req.body.password, "Staff"],
+      function (err) {
+        if (err) {
+          return console.log(err.message);
+        }
+        console.log("New user has been added");
+        res.send("New user has been added into the database with ID = " + req.body.userName);
+      });
 
   });
 });
 
 // Get User By ID
-app.post('/view', function (req, res) {
+app.get('/ViewUser/:Id', function (req, res) {
   db.serialize(() => {
-    db.each('SELECT id ID, name NAME FROM emp WHERE id =?', [req.body.id], function (err, row) {
-      //db.each() is only one which is funtioning while reading data from the DB
-      /* var empid= row.ID;
-     if ( empid ===" ")
-     {
-      res.send("No such record exists");
-      return console.error(err.message);
-     } */
-
+    db.each('SELECT id ID, userName NAME, userGroup UserGroup FROM emp WHERE id =?', [req.body.id], function (err, row) {     //db.each() is only one which is funtioning while reading data from the DB
       if (err) {
         res.send("Error encountered while displaying");
         return console.error(err.message);
       }
-      res.send(` ID: ${row.ID},    Name: ${row.NAME}`);
-      console.log("Entry displayed successfully");
+      res.send(` ID: ${row.ID},    Name: ${row.NAME}, User Group: ${row.UserGroup}`);
+      console.log("Details for the requested User Id is  displayed successfully");
     });
   });
 });
 
+// Get All Users
+app.get('/ViewAllUsers', function (req, res) {
+  db.serialize(() => {
+    db.each('SELECT id ID, userName NAME, userGroup UserGroup FROM user?', function (err, row) 
+    {     
+      if (err) {
+        res.send("Error encountered while displaying");
+        return console.error(err.message);
+      }
+      res.send(` ID: ${row.ID},    Name: ${row.NAME}, User Group: ${row.UserGroup}`);
+      console.log("Deatails for all Users are displayed successfully");
+    });
+  });
+});
+
+//Update Password
+app.put('/Update/Password/:id', function (req, res) {
+  db.serialize(() => {
+    db.run('UPDATE user SET password = ? WHERE id = ?', [req.body.password, req.body.id], function (err) {
+      if (err) {
+        res.send("Error encountered while updating");
+        return console.error(err.message);
+      }
+      res.send("User Password updated successfully");
+      console.log("User Password updated successfully");
+    });
+  });
+});
+
+//Update User Group
+app.put('/Update/UserGroup/:id', function (req, res) {
+  db.serialize(() => {
+    db.run('UPDATE user SET userGroup = ? WHERE id = ?', [req.body.UserGroup, req.body.id], function (err) {
+      if (err) {
+        res.send("Error encountered while updating");
+        return console.error(err.message);
+      }
+      res.send("User Group updated successfully");
+      console.log("User Group updated successfully");
+    });
+  });
+});
+
+// Delete User
+app.delete('/Delete/ :id', function (req, res) {
+  db.serialize(() => {
+    db.run('DELETE FROM user WHERE id = ?', req.body.id, function (err) {
+      if (err) {
+        res.send("Error encountered while deleting");
+        return console.error(err.message);
+      }
+      res.send("User deleted");
+      console.log("User deleted");
+    });
+  });
+
+});
 
 //  --------------------------------------------------- End User CRUD Functions -----------------------------------------------------------------------
 
 //  --------------------------------------------------- Begin Inventory Loan CRUD Functions -----------------------------------------------------------------------
+
+//Add New Device
+app.post('/AddDevice', function (req, res) {
+  db.serialize(() => {
+    var deviceCount = "SELECT COUNT(*) FROM devices;";
+    if (deviceCount = 10)
+    {
+      console.log("A new device cannot be added as the inventory already has the maximum number of devices");
+    }
+    db.run('INSERT INTO devices(device,os,manufacturer,lastCheckedOutDate,lastCheckedOutBy,isCheckedOut) VALUES(?,?,?,?,?,?)',
+      [req.body.device, req.body.os, req.body.manufacturer,null, null, "false"],
+      function (err) {
+        if (err) {
+          return console.log(err.message);
+        }
+        console.log("New device has been added");
+        res.send("New user has been added into the database with ID = " + req.body.userName);
+      });
+
+  });
+});
+
+// Get All Devices
+app.get('/ViewAllDevices', function (req, res) {
+  db.serialize(() => {
+    db.each('SELECT device Device,os OS,manufacturer Manufacturer,lastCheckedOutDate LastCheckedOutDate,lastCheckedOutBy LastCheckedOutBy,isCheckedOut IsCheckedOut FROM devices?', function (err, row) 
+    {    
+      if (err) {
+        res.send("Error encountered while displaying");
+        return console.error(err.message);
+      }
+      res.send(`ID: ${row.ID}, Device: ${row.device}, OS: ${row.os}, Manufacturer: ${row.manufacturer}, Last Checked Out Date: ${row.lastCheckedOutDate}, Last Checked Out By: ${row.lastCheckedOutBy}, Is Checked Out: ${row.isCheckedOut}`);
+      console.log("Details for all devices are displayed successfully");
+    });
+  });
+});
+
+// Get Device By ID
+app.get('/ViewDevice/:Id', function (req, res) {
+  db.serialize(() => {
+    db.each('SELECT device,os,manufacturer,lastCheckedOutDate,lastCheckedOutBy,isCheckedOut FROM devices WHERE id =?', [req.body.id], function (err, row) {     //db.each() is only one which is funtioning while reading data from the DB
+      if (err) {
+        res.send("Error encountered while displaying");
+        return console.error(err.message);
+      }
+      res.send(` ID: ${row.ID}, Device: ${row.device}, OS: ${row.os}, Manufacturer: ${row.manufacturer}, Last Checked Out Date: ${row.lastCheckedOutDate}, Last Checked Out By: ${row.lastCheckedOutBy}, Is Checked Out: ${row.isCheckedOut}`);
+      console.log("Details for the requested device Id is  displayed successfully");
+    });
+  });
+});
+
+// Get Checked Out Devices
+app.get('/ViewCheckedOutDevice', function (req, res) {
+  db.serialize(() => {
+    db.each('SELECT device,os,manufacturer,lastCheckedOutDate,lastCheckedOutBy,isCheckedOut FROM devices WHERE id =?', ["true"], function (err, row) {     //db.each() is only one which is funtioning while reading data from the DB
+      if (err) {
+        res.send("Error encountered while displaying");
+        return console.error(err.message);
+      }
+      res.send(` ID: ${row.ID}, Device: ${row.device}, OS: ${row.os}, Manufacturer: ${row.manufacturer}, Last Checked Out Date: ${row.lastCheckedOutDate}, Last Checked Out By: ${row.lastCheckedOutBy}, Is Checked Out: ${row.isCheckedOut}`);
+      console.log("Details for the requested device Id is  displayed successfully");
+    });
+  });
+});
+
+// Get Not Checked Out Devices
+app.get('/ViewNotCheckedOutDevice', function (req, res) {
+  db.serialize(() => {
+    db.each('SELECT device,os,manufacturer,lastCheckedOutDate,lastCheckedOutBy,isCheckedOut FROM devices WHERE id =?', ["false"], function (err, row) {     //db.each() is only one which is funtioning while reading data from the DB
+      if (err) {
+        res.send("Error encountered while displaying");
+        return console.error(err.message);
+      }
+      res.send(` ID: ${row.ID}, Device: ${row.device}, OS: ${row.os}, Manufacturer: ${row.manufacturer}, Last Checked Out Date: ${row.lastCheckedOutDate}, Last Checked Out By: ${row.lastCheckedOutBy}, Is Checked Out: ${row.isCheckedOut}`);
+      console.log("Details for the requested device Id is  displayed successfully");
+    });
+  });
+});
 
 //  --------------------------------------------------- End Inventory Loan CRUD Functions -----------------------------------------------------------------------
 
